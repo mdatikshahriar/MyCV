e95a8a42c9de9a8b2752c8c4de8e19b874cf3e5e
mdatikshahriar <md.atikshahriar728@gmail.com>
2021-05-18 14:38:33 +0600

    used object and array destructuring in weather.js

--- v3/javaScript/weather.js
+++ v3/javaScript/weather.js
@@ -1,15 +1,18 @@
 $(document).ready(() => {
 
-    const api = 'https://api.openweathermap.org/data/2.5/weather?lat=24.911944&lon=91.831250&appid=115d54f3a041ee26a278721da1902672';
+    const api = 'https://api.openweathermap.org/data/2.5/weather?lat=24.9119&lon=91.8313&appid=115d54f3a041ee26a278721da1902672';
 
     $.get(api)
     .then((response) => {
-        const location_temp = `${response['name']}, ${response['sys']['country']} - (${(response['main']['temp'] - 273.15).toFixed(2)}\u00B0C)`;
-        const condition = `: ${response['weather'][0]['main']}, ${response['weather'][0]['description']}`;
-        const feels_like = `: ${(response['main']['feels_like'] - 273.15).toFixed(2)}\u00B0C`;
-        const pressure = `: ${response['main']['pressure']} mb`;
-        const humidity = `: ${response['main']['humidity']}%`;
-        const wind_speed = `: ${response['wind']['speed']} km/h`;
+        let { name, sys: { country }, weather: [ { main: main_condition, description } ],
+        main: { temp, feels_like, pressure, humidity }, wind: { speed: wind_speed} } = response;
+
+        const location_temp = `${name}, ${country} ~ (${(temp - 273.15).toFixed(2)}\u00B0C)`;
+        const condition = `: ${main_condition}, ${description}`;
+        feels_like = `: ${(feels_like - 273.15).toFixed(2)}\u00B0C`;
+        pressure = `: ${pressure} mb`;
+        humidity = `: ${humidity}%`;
+        wind_speed = `: ${wind_speed} km/h`;
         
         $("#location-temp").html(location_temp);
         $("#condition").html(condition);
